f3527788814b2170a20a8c6241f37a28044ed9e1
nialov <nikolasovaskainen@gmail.com>
2023-03-14 23:27:01 +0200

    fix(flake): remove library from packages

--- flake.nix
+++ flake.nix
@@ -205,8 +205,7 @@
               homer taskfzf pathnames backupper wiki-builder wsl-open-dynamic
               pretty-task kibitzr ytdl-sub bootstrapSecretsScript tasklite-core
               comma-update-flag rstcheck copier tmuxp
-              pre-commit-hook-ensure-sops deploy-rs clean-git-branches-script
-              library;
+              pre-commit-hook-ensure-sops deploy-rs clean-git-branches-script;
           };
         });
 
