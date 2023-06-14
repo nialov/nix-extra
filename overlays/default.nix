7b38e7d9792a76d2c89bdcc0dcbc2c7f2f12af3d
nialov <nikolasovaskainen@gmail.com>
2023-06-14 13:19:37 +0300

    feat: add doit-ext to overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,12 @@
 # final is the final product, prev is before applying this overlay
 # final: prev: {
-inputs: final: prev: {
+inputs: final: prev:
+
+let
+
+  inherit (prev) system;
+
+in {
   # Added to nixpkgs
   # gitmux = prev.callPackage ././packages/gitmux { };
   homer = prev.callPackage ././packages/homer { inherit inputs; };
@@ -47,6 +53,7 @@ inputs: final: prev: {
         python-final.callPackage ././packages/gazpacho { inherit inputs; };
       kibitzr =
         python-final.callPackage ././packages/kibitzr { inherit inputs; };
+      inherit (inputs.doit-ext-src.packages."${system}") doit-ext;
     })
   ];
   python3 = let
