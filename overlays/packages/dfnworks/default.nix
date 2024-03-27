297f8d9feabe19ad9de58946b79b7c39278c0460
nialov <nikolasovaskainen@gmail.com>
2024-03-27 14:32:32 +0200

    fix: continue packaging dfnworks

--- overlays/packages/dfnworks/default.nix
+++ overlays/packages/dfnworks/default.nix
@@ -1,31 +1,15 @@
-{ inputs, lib, stdenv, python3, lagrit, fehm, pflotran }:
+{ inputs, lib, stdenv, python3, lagrit, fehm, pflotran, symlinkJoin, makeWrapper
+, petsc, lndir, texlive }:
 
 let
 
-  dfnworks-base = stdenv.mkDerivation rec {
-    pname = "dfnworks-base";
+  dfnworks-core = stdenv.mkDerivation {
+    pname = "dfnworks-core";
     version = "unstable";
 
     src = inputs.dfnworks-src;
 
     buildInputs = [
-      (python3.withPackages (p:
-        with p; [
-          pybind11
-          wheel
-          pip
-          setuptools
-          numpy
-          scipy
-          matplotlib
-          mplstereonet
-          seaborn
-          fpdf
-          pyvtk
-          pflotran.passthru.h5py
-          networkx
-          mpmath
-        ]))
       lagrit
       fehm
       pflotran
@@ -36,6 +20,7 @@ let
     preBuild = ''
       makeFiles=(
           "DFNGen"
+          "DFNTrans"
       )
     '';
 
@@ -61,6 +46,84 @@ let
 
       # Copy libraries.
       install -Dm644 -t $out/lib/        DFNGen/*.o
+      install -Dm644 -t $out/lib/        DFNTrans/*.o
+
+      # Copy binaries
+      install -Dt $out/bin DFNGen/DFNGen
+      install -Dt $out/bin DFNTrans/DFNTrans
+
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+      description = "";
+      homepage = "https://dfnworks.lanl.gov/";
+      license = licenses.gpl3Only;
+      maintainers = with maintainers; [ nialov ];
+    };
+  };
+
+  dfnworks = let
+
+    pythonEnv = python3.withPackages (p:
+      with p; [
+        pybind11
+        wheel
+        pip
+        setuptools
+        numpy
+        scipy
+        matplotlib
+        mplstereonet
+        seaborn
+        fpdf
+        pyvtk
+        pflotran.passthru.h5py
+        networkx
+        mpmath
+      ]);
+
+  in stdenv.mkDerivation {
+    pname = "dfnworks";
+    version = "unstable";
+
+    src = inputs.dfnworks-src;
+
+    buildInputs = [
+      # lagrit
+      fehm
+      # pflotran
+
+    ];
+
+    PYTHON_EXE = "${pythonEnv}/bin/python3";
+    LAGRIT_EXE = "${lagrit}/bin/lagrit";
+    PFLOTRAN_EXE = "${pflotran}/bin/pflotran";
+    FEHM_EXE = "${fehm}/bin/fehm";
+    dfnworks_PATH = "$NIX_BUILD_TOP/$sourceRoot";
+
+    postPatch = ''
+      pushd $NIX_BUILD_TOP/$sourceRoot/pydfnworks/bin
+      ${pythonEnv}/bin/python3 fix_paths.py
+      popd
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+
+      pushd $NIX_BUILD_TOP/$sourceRoot/pydfnworks/
+      ${pythonEnv}/bin/python3 setup.py bdist_wheel
+      popd
+
+      runHook postBuild
+    '';
+    # Install as Python package
+    installPhase = ''
+      runHook preInstall
+
+      pushd $NIX_BUILD_TOP/$sourceRoot/pydfnworks/dist
+      ${pythonEnv}/bin/python3 -m pip install ./*.whl --no-index --no-warn-script-location --prefix="$out" --no-cache
+      popd
 
       runHook postInstall
     '';
@@ -103,5 +166,86 @@ let
       maintainers = with maintainers; [ nialov ];
     };
   };
+  dfnworks-python = python3.pkgs.buildPythonPackage {
+    pname = "dfnworks";
+    version = "unstable";
 
-in dfnworks-base
+    src = inputs.dfnworks-src;
+
+    nativeBuildInputs = [ python3.pkgs.setuptools ];
+
+    propagatedBuildInputs = with python3.pkgs; [
+      numpy
+      scipy
+      matplotlib
+      mplstereonet
+      fpdf
+      pyvtk
+      pflotran.passthru.h5py
+      networkx
+      seaborn
+      mpmath
+    ];
+
+    checkInputs = with python3.pkgs; [ nose ];
+
+    postPatch = ''
+      substituteInPlace ./pydfnworks/pydfnworks/release.py \
+        --replace "datetime" ""
+      cd ./pydfnworks
+    '';
+
+    meta = with lib; {
+      description = "";
+      homepage = "https://dfnworks.lanl.gov/";
+      license = licenses.gpl3Only;
+      maintainers = with maintainers; [ nialov ];
+    };
+  };
+
+  dfnworks-full = let pythonEnv = python3.withPackages (_: [ dfnworks-python ]);
+  in stdenv.mkDerivation {
+    name = "dfnworks-full";
+    unpackPhase = ''
+      mkdir $out
+      ${lndir}/bin/lndir -silent ${pythonEnv} $out
+      # install --directory ${pythonEnv} $src
+      # export src=$(mktemp -d)
+      # ls -la ${pythonEnv}
+      # exit 1
+      # cp -r ${pythonEnv} $src
+      # ls -la $src
+    '';
+    buildInputs = [ makeWrapper ];
+    postBuild = let
+      PYTHON_EXE = "${pythonEnv}/bin/python3";
+      LAGRIT_EXE = "${lagrit}/bin/lagrit";
+      PFLOTRAN_EXE = "${pflotran}/bin/pflotran";
+      FEHM_EXE = "${fehm}/bin/fehm";
+      DFNGEN_EXE = "${dfnworks-core}/bin/DFNGen";
+      dfnworks_PATH = inputs.dfnworks-src;
+      wraps = [
+        "--set PYTHON_EXE ${PYTHON_EXE}"
+        "--set LAGRIT_EXE ${LAGRIT_EXE}"
+        "--set PFLOTRAN_EXE ${PFLOTRAN_EXE}"
+        "--set FEHM_EXE ${FEHM_EXE}"
+        "--set DFNGEN_EXE ${DFNGEN_EXE}"
+        "--set dfnworks_PATH ${dfnworks_PATH}"
+        "--prefix PATH : ${lib.makeBinPath [ texlive.combined.scheme-full ]}"
+      ];
+    in ''
+      wrapProgram $out/bin/python3 ${builtins.concatStringsSep " " wraps}
+      $out/bin/python3 --help > /dev/null
+    '';
+
+    doInstallCheck = true;
+    installCheckPhase = ''
+      check_dir=$(mktemp -d)
+      pushd $check_dir
+      $out/bin/python3 ${inputs.dfnworks-src}/examples/constant/driver.py
+      popd
+    '';
+
+  };
+
+in dfnworks-full
--- overlays/packages/lagrit/default.nix
+++ overlays/packages/lagrit/default.nix
@@ -1,18 +1,40 @@
-{ inputs, lib, stdenv, gfortran, cmake }:
+{ inputs, lib, stdenv, gfortran, cmake, python3, runCommand, lndir, applyPatches
+}:
 
-stdenv.mkDerivation {
-  pname = "lagrit";
-  version = "3.3.2";
+let
 
-  src = inputs.lagrit-src;
+  patchedSrc = runCommand "patched-src" { } "";
 
-  nativeBuildInputs = [ gfortran cmake ];
+  self =
 
-  meta = with lib; {
-    description =
-      "Los Alamos Grid Toolbox (LaGriT) is a library of user callable tools that provide mesh generation, mesh optimization and dynamic mesh maintenance in two and three dimensions";
-    homepage = "https://github.com/lanl/lagrit";
-    license = with licenses; [ bsd3 ];
-    maintainers = with maintainers; [ nialov ];
-  };
-}
+    stdenv.mkDerivation {
+      pname = "lagrit";
+      version = "3.3.2";
+
+      # TODO: To include Exodus, use -DLAGRIT_BUILD_EXODUS=ON
+      # Exodus is from https://github.com/sandialabs/seacas but it is not packaged with nix
+      nativeBuildInputs = [ gfortran cmake ];
+
+      # TODO: Move to installCheckPhase
+      passthru.tests = runCommand "lagrit-tests" { } ''
+        mkdir $out
+        ${lndir}/bin/lndir -silent ${inputs.lagrit-src}/test $out
+        pushd $out
+        ${python3}/bin/python3 runtests.py --executable ${self}/bin/lagrit --levels 1 || exit 0
+        popd
+
+      '';
+
+      # doInstallCheck = true;
+      # installCheckPhase = ''
+      # '';
+
+      meta = with lib; {
+        description =
+          "Los Alamos Grid Toolbox (LaGriT) is a library of user callable tools that provide mesh generation, mesh optimization and dynamic mesh maintenance in two and three dimensions";
+        homepage = "https://github.com/lanl/lagrit";
+        license = with licenses; [ bsd3 ];
+        maintainers = with maintainers; [ nialov ];
+      };
+    };
+in self
