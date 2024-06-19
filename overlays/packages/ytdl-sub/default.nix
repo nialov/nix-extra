093aa00fd1256aace33c2c98e4acf67cd48e624d
nialov <nikolasovaskainen@gmail.com>
2024-06-19 10:17:52 +0300

    fix(ytdl-sub): update build tooling
    Now uses pyproject.toml.

--- overlays/packages/ytdl-sub/default.nix
+++ overlays/packages/ytdl-sub/default.nix
@@ -1,35 +1,19 @@
-{ inputs, lib, python3, ffmpeg, writeText }:
+{ inputs, lib, python3, ffmpeg }:
 
 python3.pkgs.buildPythonApplication {
   pname = "ytdl-sub";
   version = inputs.ytdl-sub-src.shortRev;
-  format = "setuptools";
+  format = "pyproject";
 
   src = inputs.ytdl-sub-src;
 
-  # Add dummy setup.py, remove argparse and strict dependency versions
   # Add ffmpeg from nix to default path
-  postPatch = let
-    setupPyText = ''
-      from setuptools import setup
-
-      setup()
-    '';
-    setupPy = writeText "setup.py" setupPyText;
-  in ''
+  postPatch = ''
     substituteInPlace src/ytdl_sub/config/defaults.py \
         --replace 'DEFAULT_FFMPEG_PATH = "/usr/bin/ffmpeg' \
         'DEFAULT_FFMPEG_PATH = "${ffmpeg}/bin/ffmpeg' \
         --replace 'DEFAULT_FFPROBE_PATH = "/usr/bin/ffprobe"' \
         'DEFAULT_FFPROBE_PATH = "${ffmpeg}/bin/ffprobe"'
-    substituteInPlace setup.cfg \
-        --replace 'argparse==1.4.0' "" \
-        --replace 'mergedeep==1.3.4' 'mergedeep' \
-        --replace 'mediafile==0.12.0' 'mediafile' \
-        --replace 'PyYAML==5.3.1' 'PyYAML' \
-        --replace 'yt-dlp==2023.7.6' 'yt-dlp' \
-        --replace 'colorama==0.4.6,' 'colorama'
-    cp ${setupPy} setup.py
   '';
 
   propagatedBuildInputs = with python3.pkgs; [
@@ -41,6 +25,12 @@ python3.pkgs.buildPythonApplication {
   ];
 
   buildInputs = [ ffmpeg ];
+  nativeBuildInputs = [
+    python3.pkgs.setuptools
+    python3.pkgs.wheel
+    python3.pkgs.pythonRelaxDepsHook
+  ];
+  pythonRelaxDeps = true;
 
   pythonImportsCheck = [ "ytdl_sub" ];
 
