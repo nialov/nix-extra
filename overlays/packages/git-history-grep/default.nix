31f736ba3488ccebe8aed4e8c39cbdec97a5961d
nialov <nikolasovaskainen@gmail.com>
2024-08-23 09:36:50 +0300

    feat(packages): add git-history-grep

---
+++ overlays/packages/git-history-grep/default.nix
@@ -0,0 +1,31 @@
+{ stdenv, python3, installShellFiles, git, lib, makeWrapper }:
+
+stdenv.mkDerivation {
+  name = "git-history-grep";
+  nativeBuildInputs = [ installShellFiles ];
+  buildInputs = [
+    (python3.withPackages (pythonPackages: with pythonPackages; [ typer rich ]))
+    makeWrapper
+  ];
+  unpackPhase = "true";
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${././git_history_grep.py} $out/bin/git-history-grep
+    wrapProgram $out/bin/git-history-grep --prefix PATH : ${
+      lib.makeBinPath [ git ]
+    }
+    chmod +x $out/bin/git-history-grep
+  '';
+  postFixup = ''
+    INSTALL_DIR=$(mktemp -d)
+    $out/bin/git-history-grep --show-completion fish > $INSTALL_DIR/completion.fish
+    $out/bin/git-history-grep --show-completion bash > $INSTALL_DIR/completion.bash
+    installShellCompletion --name git-history-grep.bash --bash $INSTALL_DIR/completion.bash
+    installShellCompletion --name git-history-grep.fish --fish $INSTALL_DIR/completion.fish
+  '';
+  doInstallCheck = true;
+  installCheckPhase = ''
+    $out/bin/git-history-grep --help
+  '';
+
+}
