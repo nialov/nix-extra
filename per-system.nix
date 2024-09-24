dc2639f88786a456f87534802b29ba0f5878d1a4
nialov <nikolasovaskainen@gmail.com>
2024-09-24 16:29:32 +0300

    feat(per-system): add fhs envs

--- per-system.nix
+++ per-system.nix
@@ -49,7 +49,7 @@
             sync-git-tag-with-poetry resolve-version poetry-run tracerepo
             python39-with-c-tooling python310-with-c-tooling
             python311-with-c-tooling jupytext-nb-edit template-check nvim-nixvim
-            git-history-grep;
+            git-history-grep micromamba-fhs-env geo-fhs-env;
           inherit (pkgs.vimPlugins) neoai-nvim;
           inherit (pkgs.python3Packages)
             doit-ext sphinxcontrib-mermaid sphinx-gallery pandera bubop
