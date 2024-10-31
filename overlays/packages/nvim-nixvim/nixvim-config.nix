30e92d072c600ed97ca8aa723722811eb170f332
nialov <nikolasovaskainen@gmail.com>
2024-10-31 16:13:53 +0200

    fix(nvim-nixvim,cmp): improve cmp menu formatting

--- overlays/packages/nvim-nixvim/nixvim-config.nix
+++ overlays/packages/nvim-nixvim/nixvim-config.nix
@@ -134,29 +134,29 @@ in {
       pattern = [ "*" ];
       command = "wincmd =";
     }
-    {
-      event = [ "FocusLost" ];
-      pattern = [ "*" ];
-      callback.__raw = ''
-        function ()
-            vim.cmd [[
-                " Save when losing focus.
-                exe ':silent! update'
+    # {
+    #   event = [ "FocusLost" ];
+    #   pattern = [ "*" ];
+    #   callback.__raw = ''
+    #     function ()
+    #         vim.cmd [[
+    #             " Save when losing focus.
+    #             exe ':silent! update'
 
-                " Go back to normal mode from insert mode.
-                if mode() == 'i'
-                  exe ':stopinsert'
-                endif
+    #             " Go back to normal mode from insert mode.
+    #             if mode() == 'i'
+    #               exe ':stopinsert'
+    #             endif
 
-                if getbufvar(bufnr('%'), '&filetype') == 'fzf'
-                  exe ':q'
-                endif
-            ]]
+    #             if getbufvar(bufnr('%'), '&filetype') == 'fzf'
+    #               exe ':q'
+    #             endif
+    #         ]]
 
-        end
+    #     end
 
-      '';
-    }
+    #   '';
+    # }
   ];
   userCommands = {
     "WikiEntry" = {
@@ -477,6 +477,55 @@ in {
           debounce = 240;
           fetching_timeout = 1000;
           throttle = 60;
+        };
+        formatting = {
+          fields = [ "abbr" "menu" "kind" ];
+          format.__raw = ''
+            function(entry, item)
+                    -- Define menu shorthand for different completion sources.
+                    local menu_icon = {
+                        nvim_lsp = "💻",
+                        nvim_lua = "🌙",
+                        luasnip  = "",
+                        buffer   = "🔋",
+                        path     = "💾",
+                    }
+                    -- Set the menu "icon" to the shorthand for each completion source.
+                    item.menu = menu_icon[entry.source.name] or entry.source_name
+
+                    -- Set the fixed width of the completion menu to 60 characters.
+                    -- fixed_width = 20
+
+                    -- Set 'fixed_width' to false if not provided.
+                    fixed_width = fixed_width or false
+
+                    -- Get the completion entry text shown in the completion window.
+                    local content = item.abbr
+
+                    -- Set the fixed completion window width.
+                    if fixed_width then
+                        vim.o.pumwidth = fixed_width
+                    end
+
+                    -- Get the width of the current window.
+                    local win_width = vim.api.nvim_win_get_width(0)
+
+                    -- Set the max content width based on either: 'fixed_width'
+                    -- or a percentage of the window width, in this case 20%.
+                    -- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+                    local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+                    -- Truncate the completion entry text if it's longer than the
+                    -- max content width. We subtract 3 from the max content width
+                    -- to account for the "..." that will be appended to it.
+                    if #content > max_content_width then
+                        item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+                    else
+                        item.abbr = content .. (" "):rep(max_content_width - #content)
+                    end
+                    return item
+                end
+          '';
         };
       };
       cmdline = {
