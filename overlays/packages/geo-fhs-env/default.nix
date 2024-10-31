a58f7fa4dcd56cfa30bc0ea00d95323432ea155f
nialov <nikolasovaskainen@gmail.com>
2024-10-31 10:21:06 +0200

    feat(geo-fhs-env): improve for more general use

--- overlays/packages/geo-fhs-env/default.nix
+++ overlays/packages/geo-fhs-env/default.nix
@@ -1,24 +1,41 @@
-{ buildFHSUserEnv, fish, gdal, poetry, python39, python310, python311, cacert
-, stdenv, gcc, expat, zlib, ... }:
+{ buildFHSUserEnv, lib, writeScript, ... }:
 buildFHSUserEnv {
   name = "geo-fhs-env";
 
-  targetPkgs = _: [ fish gdal poetry python39 python310 python311 cacert gcc ];
-  runScript = "bash";
+  targetPkgs = fhsPkgs:
+
+    let
+      pythons = builtins.map
+        (python: python.withPackages (p: [ p.setuptools p.pip p.wheel ]))
+        (lib.attrValues { inherit (fhsPkgs) python310 python311; });
+    in lib.attrValues {
+      inherit (fhsPkgs) fish poetry cacert gcc expat zlib pkg-config cmake uv;
+      glibDev = fhsPkgs.glib.dev;
+      libffiDev = fhsPkgs.libffi.dev;
+
+    } ++ pythons;
+  runScript = writeScript "entrypoint.sh" ''
+    #!/usr/bin/env bash
+
+    exec /usr/bin/env fish "$@"
+  '';
 
   profile =
 
-    let
-      ccLib = "${stdenv.cc.cc.lib}/lib";
-      zlibLib = "${zlib}/lib";
-      expatLib = "${expat}/lib";
-      ldPath = "${ccLib}:${zlibLib}:${expatLib}";
+    # let
+    #   ccLib = "${stdenv.cc.cc.lib}/lib";
+    #   zlibLib = "${zlib}/lib";
+    #   expatLib = "${expat}/lib";
+    #   ldPath = "${ccLib}:${zlibLib}:${expatLib}";
 
-    in ''
-      set -e
+    # in 
+    # ''
+    #   export POETRY_VIRTUALENVS_IN_PROJECT=1
+    #   export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"${ldPath}"
+    # '';
+    ''
       export POETRY_VIRTUALENVS_IN_PROJECT=1
-      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":"${ldPath}"
-      exec fish --no-config
-      set +e
+      export UV_LINK_MODE=copy
+      export UV_PYTHON_PREFERENCE=only-managed
     '';
 }
