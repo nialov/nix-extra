258ef611a750c2f25b4132a1f6c6593af9e5c5a7
nialov <nikolasovaskainen@gmail.com>
2025-02-12 16:18:50 +0200

    build(per-system): remove deprecated fhs packages

--- per-system.nix
+++ per-system.nix
@@ -46,7 +46,7 @@
             sync-git-tag-with-poetry resolve-version poetry-run
             python39-with-c-tooling python310-with-c-tooling
             python311-with-c-tooling jupytext-nb-edit template-check nvim-nixvim
-            git-history-grep micromamba-fhs-env geo-fhs-env gdal;
+            git-history-grep gdal;
           inherit (pkgs.python3Packages)
             doit-ext sphinxcontrib-mermaid sphinx-gallery item-synchronizer
             gkeepapi powerlaw frackit python-ternary mplstereonet pyvtk;
