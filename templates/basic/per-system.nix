0b3de511def8cb0eb895c755a0f7aa2f205d26ac
nialov <nikolasovaskainen@gmail.com>
2025-02-13 13:33:47 +0200

    feat(templates,basic): add to shell hook
    Prefixes PATH with defined Python env so that those binaries are called first
    in shell exexutions. Add directories to PYTHONPATH so Python can import from
    them.

--- templates/basic/per-system.nix
+++ templates/basic/per-system.nix
@@ -6,7 +6,16 @@
         mkNixpkgs = nixpkgs:
           import nixpkgs {
             inherit system;
-            overlays = [ inputs.nix-extra.overlays.default ];
+            overlays =
+
+              let
+                localOverlay = _: prev: {
+                  python-env = prev.python3.withPackages
+                    (p: lib.attrValues { inherit (p) numpy; });
+
+                };
+
+              in [ inputs.nix-extra.overlays.default localOverlay ];
             config = { allowUnfree = true; };
           };
 
@@ -14,9 +23,13 @@
         _module.args.pkgs = mkNixpkgs inputs.nixpkgs;
         devShells = {
           default = pkgs.mkShell {
-            buildInputs = lib.attrValues { };
+            buildInputs = lib.attrValues { inherit (pkgs) python-env; };
             shellHook = config.pre-commit.installationScript + ''
               export PROJECT_DIR="$PWD"
+              # Prefix PATH with defined Python environment binaries
+              export PATH="${pkgs.python-env}/bin/:$PATH"
+              # Prefix PYTHONPATH with ./ and potential ./src/
+              export PYTHONPATH="$PWD:$PWD/src:$PYTHONPATH"
             '';
           };
 
