f3fb1351f3f8e31715a7ddfb79b0ab1e40211eaa
nialov <nikolasovaskainen@gmail.com>
2025-04-06 12:25:16 +0300

    feat(nvim-nixvim,blink-cmp): configure codestral

--- overlays/packages/nvim-nixvim/plugins/blink-cmp.nix
+++ overlays/packages/nvim-nixvim/plugins/blink-cmp.nix
@@ -73,14 +73,14 @@
     config = ''
       lua << EOF
       require("minuet").setup({
-          provider = 'openai_fim_compatible',
-          n_completions = 1, -- recommend for local model for resource saving
+          provider = 'codestral',
+          -- n_completions = 1, -- recommend for local model for resource saving
           -- I recommend beginning with a small context window size and incrementally
           -- expanding it, depending on your local computing power. A context window
           -- of 512, serves as an good starting point to estimate your computing
           -- power. Once you have a reliable estimate of your local computing power,
           -- you should adjust the context window to a larger value.
-          context_window = 512,
+          -- context_window = 512,
           provider_options = {
               openai_fim_compatible = {
                   api_key = 'TERM',
@@ -90,6 +90,20 @@
                   optional = {
                       max_tokens = 112,
                       top_p = 0.9,
+                  },
+              },
+              codestral = {
+                  model = 'codestral-latest',
+                  end_point = 'https://codestral.mistral.ai/v1/fim/completions',
+                  api_key = 'CODESTRAL_API_KEY',
+                  stream = true,
+                  -- template = {
+                      -- prompt = "See [Prompt Section for default value]",
+                      -- suffix = "See [Prompt Section for default value]",
+                  -- },
+                  optional = {
+                      stop = nil, -- the identifier to stop the completion generation
+                      max_tokens = nil,
                   },
               },
           },
