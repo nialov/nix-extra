97e95aef0cf1eb6b8c10cc6ee3b171bbe51238d8
nialov <nikolasovaskainen@gmail.com>
2025-04-06 11:54:42 +0300

    feat(nvim-nixvim,blink-cmp): improve completion

--- overlays/packages/nvim-nixvim/plugins/blink-cmp.nix
+++ overlays/packages/nvim-nixvim/plugins/blink-cmp.nix
@@ -1,9 +1,11 @@
-{
+{ pkgs, ... }: {
   plugins = {
     blink-cmp = {
       enable = true;
       settings = {
         fuzzy.prebuilt_binaries.download = false;
+        # Recommended by minuet-ai-nvim
+        completion.trigger.prefetch_on_insert = false;
         keymap = {
           # preset = "super-tab";
           # -- set to 'none' to disable the 'default' preset
@@ -12,6 +14,7 @@
           "<C-p>" = [ "select_prev" "fallback" ];
           "<C-n>" = [ "select_next" "fallback" ];
           "<C-k>" = [ "accept" "fallback" ];
+          "<A-y>".__raw = "require('minuet').make_blink_map()";
           # ['<Down>'] = { 'select_next', 'fallback' },
 
           # -- disable a keymap from the preset
@@ -53,11 +56,47 @@
                 debug = false;
               };
             };
+            minuet = {
+              name = "minuet";
+              module = "minuet.blink";
+              score_offset = 8;
+            };
           };
         };
       };
     };
     blink-ripgrep.enable = true;
   };
+
+  extraPlugins = [{
+    plugin = pkgs.vimPlugins.minuet-ai-nvim;
+    config = ''
+      lua << EOF
+      require("minuet").setup({
+          provider = 'openai_fim_compatible',
+          n_completions = 1, -- recommend for local model for resource saving
+          -- I recommend beginning with a small context window size and incrementally
+          -- expanding it, depending on your local computing power. A context window
+          -- of 512, serves as an good starting point to estimate your computing
+          -- power. Once you have a reliable estimate of your local computing power,
+          -- you should adjust the context window to a larger value.
+          context_window = 512,
+          provider_options = {
+              openai_fim_compatible = {
+                  api_key = 'TERM',
+                  name = 'Ollama',
+                  end_point = 'https://ollama.novaskai.xyz/v1/completions',
+                  model = 'deepseek-coder-v2:16b',
+                  optional = {
+                      max_tokens = 112,
+                      top_p = 0.9,
+                  },
+              },
+          },
+      })
+      EOF
+
+    '';
+  }];
 
 }
