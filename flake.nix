69e49d39ae565391cc2a04540ff6aa292422a7e5
nialov <nikolasovaskainen@gmail.com>
2025-07-03 10:23:17 +0300

    chore(flake): remove nix-fast-build input

--- flake.nix
+++ flake.nix
@@ -236,11 +236,6 @@
       url = "github:marcharper/python-ternary";
       flake = false;
     };
-    nix-fast-build = {
-      url = "github:Mic92/nix-fast-build";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-parts.follows = "flake-parts";
-    };
     magazine-nvim-src = {
       url = "github:iguanacucumber/magazine.nvim";
       flake = false;
