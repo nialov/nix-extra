9ecc5fb66a54cdec503e4951c73751664a239498
nialov <nikolasovaskainen@gmail.com>
2025-09-05 16:58:43 +0300

    fix: migrate blink-ripgrep configuration to new format in Nix file

--- overlays/custom-packages/nvim-nixvim/plugins/blink-cmp.nix
+++ overlays/custom-packages/nvim-nixvim/plugins/blink-cmp.nix
@@ -128,11 +128,15 @@
               score_offset = -10;
               opts = {
                 prefix_min_len = 3;
-                context_size = 5;
-                max_filesize = "1M";
+                backend = {
+                  context_size = 5;
+                  ripgrep = {
+                    max_filesize = "1M";
+                    project_root_fallback = true;
+                    search_casing = "--ignore-case";
+                  };
+                };
                 project_root_marker = ".git";
-                project_root_fallback = true;
-                search_casing = "--ignore-case";
                 additional_rg_options = { };
                 fallback_to_regex_highlighting = true;
                 ignore_paths = { };
